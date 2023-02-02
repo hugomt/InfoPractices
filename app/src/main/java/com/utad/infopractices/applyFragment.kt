c6456cb708ed5aa437996497a4be0df6bf155530
marti <hugo.martinez@live.u-tad.com>
2023-02-02 12:08:34 +0100

    nueva pantalla

--- app/src/main/java/com/utad/infopractices/applyFragment.kt
+++ app/src/main/java/com/utad/infopractices/applyFragment.kt
@@ -10,12 +10,18 @@ import androidx.appcompat.app.AppCompatActivity
 
 class applyFragment : Fragment() {
 
+    private lateinit var cardData: Practices
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_apply, container, false)
         val arrowImage = view.findViewById<ImageView>(R.id.arrowImage)
+        val imageView = view.findViewById<ImageView>(R.id.empresaimage)
+        cardData = requireArguments().getParcelable("pratices")!!
+
+        imageView.setImageResource(cardData.Image)
 
         return view
     }
