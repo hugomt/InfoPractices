07382246ec28e76d0693e56aba0ce08d0c3ca147
marti <hugo.martinez@live.u-tad.com>
2023-02-02 19:45:32 +0100

    pantalla apply hecha

--- app/src/main/java/com/utad/infopractices/applyFragment.kt
+++ app/src/main/java/com/utad/infopractices/applyFragment.kt
@@ -1,18 +1,18 @@
 package com.utad.infopractices
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.method.PasswordTransformationMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
-import android.widget.Spinner
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 
+s
 class applyFragment : Fragment() {
-
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,7 +21,11 @@ class applyFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_apply, container, false)
 
         val arrowImage = view.findViewById<ImageView>(R.id.arrowImage)
+        val btn = view.findViewById<Button>(R.id.sumbitButton)
 
+        btn.setOnClickListener(){
+            Toast.makeText(context, "Saved application", Toast.LENGTH_SHORT).show()
+        }
 
         return view
     }
